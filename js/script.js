5556740ddcfebf2b1a330db5aac0ff608f09ef10
Marcin SNARSKI <company.m2s@gmail.com>
2019-05-27 12:51:55 +0200

    remove class active

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 'use strict';
 
-function titleClickHandler(event){
+function titleClickHandler(event) {
   console.log('Link was clicked!');
 
   /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove('active');
+  }
 
   /* add class 'active' to the clicked link */
+  const activeArticles = document.querySelectorAll('.posts article.active');
+
+  for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove('active');
+  }
 
   /* remove class 'active' from all articles */
 
@@ -18,6 +28,6 @@ function titleClickHandler(event){
 
 const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
+for (let link of links) {
   link.addEventListener('click', titleClickHandler);
 }
