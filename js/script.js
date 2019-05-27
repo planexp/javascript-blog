f45538953fcb38fda710fa49fe7103c2e3b75f97
Marcin SNARSKI <company.m2s@gmail.com>
2019-05-27 13:20:27 +0200

    Dodanie klasy active

--- js/script.js
+++ js/script.js
@@ -1,29 +1,40 @@
 'use strict';
 
 function titleClickHandler(event) {
+  const clickedElement = this;
   console.log('Link was clicked!');
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
+
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for (let activeLink of activeLinks) {
     activeLink.classList.remove('active');
   }
 
-  /* add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
+
+
+  /* [DONE]  remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts article.active');
 
   for (let activeArticle of activeArticles) {
     activeArticle.classList.remove('active');
   }
 
-  /* remove class 'active' from all articles */
+  /*  [DONE] get 'href' attribute from the clicked link */
 
-  /* get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
 
-  /* add class 'active' to the correct article */
+  /* [DONE] add class 'active' to the correct article */
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
